1ee296d8b90e804cd610d30c7af27854fed2aa39
Tulio Leal <tuliolealdev@gmail.com>
2024-12-08 14:35:16 -0300

    Revert "Refactor theme configuration to use TypeScript type for customTheme"
    This reverts commit 4ffb7b5a2b579c3a76e0fcb22bcf449145a909c2.

--- app/themes/theme.ts
+++ app/themes/theme.ts
@@ -1,9 +1,9 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { Button, extendTheme, Select, type ThemeConfig } from '@chakra-ui/react'
 import { switchTheme } from './switch_theme'
 import { buttonTheme } from './button_theme'
 import { selectTheme } from './select_theme'
-const customTheme: ThemeConfig = extendTheme({
-  initialColorMode: "dark",
+const customTheme = extendTheme({
+  initialColorMode: 'dark',
   components: {
     Switch: switchTheme,
     Button: buttonTheme,
