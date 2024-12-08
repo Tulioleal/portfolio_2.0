08751469d3ea65c1a0990e390df99a3969ad079e
Tulioleal <tuliolealdev@gmail.com>
2024-12-08 18:54:10 -0300

    Add Projects section to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 import About from './sections/About'
 import Hero from './sections/Hero'
+import Projects from './sections/Projects'
 
 export default function Page() {
   return (
     <>
       <Hero />
       <About />
+      <Projects />
     </>
   )
 }
