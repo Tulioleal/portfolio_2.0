bd025f3d941a2dc74116761277a8b0f4bf1e3693
Tulioleal <tuliolealdev@gmail.com>
2024-12-08 12:01:02 -0300

    Add About section to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import About from './sections/About'
 import Hero from './sections/Hero'
 
 export default function Page() {
   return (
     <>
       <Hero />
+      <About />
     </>
   )
 }
