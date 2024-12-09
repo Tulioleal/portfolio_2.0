f3d69c50fba1bede13cc7ecf03d598c3ad2fdc03
Tulioleal <tuliolealdev@gmail.com>
2024-12-09 15:50:39 -0300

    Update sitemap URL to include language parameter for improved localization

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://tulioleal.info',
+      url: 'https://tulioleal./en',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
